ae9a51cc90dd58898b527c645521513c88f1b0b2
lank <2915841032@qq.com>
2024-07-08 11:14:30 +0800

    feat(新增)

---
+++ 山东省投资项目在线审批监管平台/main.py
@@ -0,0 +1,38 @@
+﻿import execjs
+import requests
+import json
+
+function_code=open('参数生成.js',encoding='utf-8-sig').read()
+data_code=execjs.compile(function_code)
+data_file=data_code.call('main123')
+headers = {
+    "Accept": "application/json, text/javascript, */*; q=0.01",
+    "Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6",
+    "Connection": "keep-alive",
+    "Content-Type": "application/json",
+    "Origin": "http://221.214.94.51:8081",
+    "Referer": "http://221.214.94.51:8081/icity/ipro/projectlist",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 Edg/126.0.0.0",
+    "X-Requested-With": "XMLHttpRequest"
+}
+
+url = data_file
+
+data = {
+    "projectcode": "",
+    "projectname": "",
+    "contractor": "",
+    "projecttype": "",
+    "page": 2,
+    "limit": 10
+}
+data = json.dumps(data, separators=(',', ':'))
+response = requests.post(url, headers=headers, data=data, verify=False)
+
+# print(response.text)
+print(response)
+data=response.json()['data']
+with open('data.json', 'w', encoding='utf-8') as f:
+    json.dump(data, f, ensure_ascii=False, indent=4)
+
+# print(response.json())
---
+++ 山东省投资项目在线审批监管平台/参数生成.js
@@ -0,0 +1,40 @@
+﻿function LExisNotNull(obj) {
+    if (obj === undefined || obj === null || obj == "null" || obj === "" || obj == "undefined")
+        return false;
+    return true;
+}
+
+function main123(){
+var __signature="ab28271720018067238"
+var curUrl = "/icity/api-v2/app.icity.ipro.IproCmd/getProjectList";
+if(true){
+    var sig = "";
+    var chars = "0123456789abcdef";
+    if(!LExisNotNull(__signature)){
+        var curTime = parseInt(Math.random()*(9999-1000+1)+1000)+""+Date.parse(new Date());
+        sig = chars.charAt(parseInt(Math.random()*(15-15+1)+10))+chars.charAt(curTime.length)+""+curTime;
+    }else{
+        sig = __signature;
+    }
+
+    var key = "";
+    var keyIndex = -1;
+    for(var i=0;i<6;i++){
+        var c=sig.charAt(keyIndex+1);
+        key +=c;
+        keyIndex = chars.indexOf(c);
+        if(keyIndex<0 || keyIndex>=sig.length){
+            keyIndex = i;
+        }
+    }
+
+    var timestamp = parseInt(Math.random()*(9999-1000+1)+1000)+"_"+key+"_"+Date.parse(new Date());
+    var t = timestamp;//LEx.azdg.encrypt(timestamp,key);
+    t = t.replace(/\+/g,"_");
+    curUrl+= "?s=" + sig;
+    curUrl+= "&t=" +  t;
+    console.log("http://221.214.94.51:8081"+curUrl)
+    return "http://221.214.94.51:8081" + curUrl;
+}
+}
+main123()
